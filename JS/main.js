3085fa802f5d8a32d3d9ebfc109e748e8de0331e
Travis Czerw <travis.czerw@gmail.com>
2015-01-19 15:10:53 -0600

    Changed Slot machine game to work on HTML entirely. Added score and win/loss ration.

--- JS/main.js
+++ JS/main.js
@@ -1,76 +1,51 @@
-var slotArray = new Array();
-
-slotArray[0] = new Image();
-slotArray[0].src = "../images/number1.jpg";
-
-slotArray[1] = new Image();
-slotArray[1].src = "../images/number2.jpg";
-
-slotArray[2] = new Image();
-slotArray[2].src = "../images/number3.jpg";
-
-
-do {
-
-	var num1 = Math.ceil(Math.random() * 3);
-	console.log(num1);
-	var num2 = Math.ceil(Math.random() * 3);
-	console.log(num2);
-	var num3 = Math.ceil(Math.random() * 3);
-	console.log(num3);
-
-	if (num1 === num2 && num1 === num3) {
-		console.log("YOU WIN!");
-		alert("You won with all "+num1+"'s");
-	}
-	else if (num1 === 1 && num2 === 2 && num3 ===3) {
-		console.log("YOU WIN!");
-		alert("You won with "+num1+" "+num2+" and "+num3);
-	}
-	else if (num1 === 3 && num2 === 2 && num3 ===1) {
-		console.log("YOU WIN!");
-		alert("You won with "+num1+" "+num2+" and "+num3);
-	}
-	else {
-	 	console.log("YOU LOSE!");
-	 	alert("You lost with "+num1+" "+num2+" and "+num3);
-	}
-
-} while (confirm("Keep playing?"));
-
+var slotArray = ["../images/number1.jpg","../images/number2.jpg","../images/number3.jpg"];
+var winCount = 0;
+var lossCount = 0;
 
 $( document ).ready(function() {
 
-	var control = Math.ceil(Math.random() * 10);
- 
+	$('.wins').html('Wins: 0');
+	$('.losses').html('Losses: 0');
+	$('.ratio-box').html('Win Ratio: 0%');
+
 	$('#play-button').click(function(event) {
 
 			var randomNum = Math.floor(Math.random() * 3);
-			console.log(randomNum);
+			var num1 = 0;
+			var num2 = 0;
+			var num3 = 0;
 
-			$('#slot1').html(slotArray[randomNum]);
-			randomNum = Math.floor(Math.random() * 3);
-			console.log(randomNum);
-			$('#slot2').html(slotArray[randomNum]);
-			// randomNum = Math.floor(Math.random() * 3);
-			// $('#slot13').html(slotArray[randomNum]);
+			$("#img1").attr("src",slotArray[randomNum]);
+			num1 = randomNum +1;
+			randomNum = Math.floor(Math.random() * 3);	
+			$("#img2").attr("src",slotArray[randomNum]);
+			num2 = randomNum +1;
+			randomNum = Math.floor(Math.random() * 3);	
+			$("#img3").attr("src",slotArray[randomNum]);
+			num3 = randomNum +1;
 
-			// $('#img1').slideUp( 900 );
+			if (num1 === num2 && num1 === num3) {
+				$('.dialouge-box').html('All '+num1+"'s, You win!");
+				winCount++;
+			}
 
-			// do {
-			// 	$('.img1').hide().delay(800);
-			// 	$('.img2').show().delay(800);
-			// 	$('.img2').hide().delay(800);
-			// 	$('.img3').show().delay(800);
-			// 	control++;
-			// } while (control < 20);
-		
-//     	$(function () {
-// 		    var parent = $(".slot-box");
-// 		    var divs = parent.children();
-// 		    while (divs.length) {
-//         	parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
-//     }
-// });
+			else if (num1 === 1 && num2 === 2 && num3 ===3) {
+				$('.dialouge-box').html('You won with 1, 2, 3, in a row!');
+				winCount++;
+			}
+
+			else if (num1 === 3 && num2 === 2 && num3 ===1) {
+				$('.dialouge-box').html('You won with 3, 2, 1 in a row!');
+				winCount++;
+			}
+
+			else {
+		 		$('.dialouge-box').html('No dice, try again!');
+		 		lossCount++;
+			}
+
+			$('.wins').html('Wins: '+winCount);
+			$('.losses').html('Losses: '+lossCount);
+			$('.ratio-box').html('Win Ratio: '+ Math.round((winCount/lossCount) * 100)+'%');
 	}); 
 });
